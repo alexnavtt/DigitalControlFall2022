edd0fbb46ffde70dedcaa2c7060509d6a9bd67bb
alexnavtt <alexnavtt@utexas.edu>
2022-11-22 14:04:34 -0600

    added a few bells and whistles to make tuning the sim easier

---
+++ InputType.m
@@ -0,0 +1,7 @@
+classdef InputType < uint8
+    enumeration
+        custom(1)
+        constant_val(2)
+        constant_acc(3)
+    end
+end
--- RunSimulation.m
+++ RunSimulation.m
@@ -16,12 +16,34 @@ tf = 30.0;                  % Simulation duration (s)
 %% Define Controller Parameters
 
 % PID parameters
-P = 1;                   % Proportional Gain
-I = 4;                      % Integral Gain
-D = 0;                      % Derivative Gain
+P = 5;                        % Proportional Gain
+I = 1;                        % Integral Gain
+D = 0;                        % Derivative Gain
 
 % Discretization (Not yet implemented)
-Ts = 0.10;                  % Sample Time (s)
+Ts = 0.10;                      % Sample Time (s)
+
+% Whether or not to include a disturbance step input in the simulation
+add_disturbance = true;         % Whether or not to add a diturbance
+disturbance_time = 10;           % Time at which to apply disturbance (s)
+disturbance_amplitude = 0.1;    % Size of disturbance (Nm)
+
+%% Create a reference input angular velocity to target
+
+% Can be "custom", "constant_val" or "constant_acc"
+input_type = uint8(InputType.constant_acc);
+
+% Custom input signal
+simin = struct();
+simin.time = (0:0.01:tf)';
+simin.signals.values = zeros(size(simin.time));
+
+% Constant input signal
+target_omega = 1;               % rad/s
+
+% Constant input acceleration 
+target_alpha = 0.1;             % rad/s^2
+max_omega    = 1;               % rad/s
 
 %% Run the simulation
 results = sim('BrushlessMotorControlSim.slx');
@@ -29,7 +51,8 @@ results = sim('BrushlessMotorControlSim.slx');
 %% Plot the results
 
 [Tw, Yw] = getSimTimeSeries(results, 'omega');
-plot(Tw, Yw)
+[Tw_star, Yw_star] = getSimTimeSeries(results, 'setpoint');
+plot(Tw, Yw, Tw_star, Yw_star)
 
 
 %% Helper Functions
